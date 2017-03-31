d70002573e4acfbcf7506ad8790b2ec3e9f94da8
Steven Kirk <grokys@gmail.com>
2017-03-31 12:22:25 +0200

    Add project files.

---
+++ LeapExtension/AdornmentFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace LeapExtension
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    internal sealed class AdornmentFactory : IWpfTextViewCreationListener
+    {
+        [Export(typeof(AdornmentLayerDefinition))]
+        [Name("TypingSpeed")]
+        [Order(After = PredefinedAdornmentLayers.Caret)]
+        [TextViewRole(PredefinedTextViewRoles.Document)]
+        public AdornmentLayerDefinition editorAdornmentLayer = null;
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+        }
+    }
+}
---
+++ LeapExtension/LeapPackage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace LeapExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [Guid(LeapPackage.PackageGuidString)]
+    public sealed class LeapPackage : Package
+    {
+        public const string PackageGuidString = "42918412-8613-44cf-909f-045bcd2b4e26";
+    }
+}
